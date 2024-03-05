6dd8d7ff1c90982252aa7a33766a3465227b4e6d
Emily Ingraham <emilyvingraham@gmail.com>
2024-03-05 14:51:01 -0700

    Added footer, links, and associated css styling

--- src/App.js
+++ src/App.js
@@ -10,8 +10,19 @@ export default function App() {
                     <h1>Dictionary Application</h1>
                 </header>
                 <main>
-                  <Search/>
+                    <Search />
                 </main>
+                <footer>
+                    This project was built by{' '}
+                    <a
+                        href="https://github.com/emilyvingraham"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Emily Ingraham
+                    </a>, is <a href="https://github.com/emilyvingraham/dictionary-application" target="_blank" rel="noreferrer">open-sourced on Github</a>, and <a href="https://react-dictionary-application-evi.netlify.app/" target="_blank" rel="noreferrer">is hosted on Netlify</a>.
+                    <p>👽</p>
+                </footer>
             </div>
         </div>
     );
