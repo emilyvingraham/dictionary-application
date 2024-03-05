0f4a3e119d4896914da64504d68548cd99f76b70
Emily Ingraham <emilyvingraham@gmail.com>
2024-03-05 15:30:16 -0700

    Integrated dictionary API into Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 export default function Search() {
-    const [word, setWord] = useState("");
+    const [word, setWord] = useState('');
+
+    function handleResponse(response) {
+        console.log(response.data);
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
-        alert(`Searching for ${word}`)
+        alert(`Searching for ${word}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        axios.get(apiUrl).then(handleResponse);
     }
-    
+
     function handleChange(event) {
         setWord(event.target.value);
     }
@@ -18,7 +26,11 @@ export default function Search() {
             <div className="row">
                 <div className="col-md-12">
                     <form onSubmit={handleSubmit}>
-                        <input type="text" className="w-50" onChange={handleChange}/>
+                        <input
+                            type="text"
+                            className="w-50"
+                            onChange={handleChange}
+                        />
                         <button type="button" className="ms-3 w-25">
                             Search
                         </button>
