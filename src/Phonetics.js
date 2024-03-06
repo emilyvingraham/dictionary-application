f455f71d8d7a4505f139b9c5886d41ee9f66ec9a
Emily Ingraham <emilyvingraham@gmail.com>
2024-03-06 14:12:53 -0700

    Added phonetic text to Phonetic.js component via map

---
+++ src/Phonetics.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Phonetics(props) {
+    return (
+        <div className="Phonetics">
+            <h4>{props.data.text}</h4>
+        </div>
+    )
+}
