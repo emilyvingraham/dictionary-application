80fff3793cbff9c07afa0cc03a310d94ed6f163f
Emily Ingraham <emilyvingraham@gmail.com>
2024-03-06 14:52:19 -0700

    Added bootstrap styling to Phonetics component button

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React from 'react';
 
 export default function Phonetics(props) {
     return (
         <div className="Phonetics d-flex justify-content-center my-2">
-            <button className="px-2 me-1 rounded-pill">▶️
-                <a className="fs-6 text-decoration-none" href={props.data.audio} target="_blank" rel="noreferrer">
-                    Listen
+            <button className="bg-transparent p-0">
+                <a
+                    className="fs-6 text-decoration-none"
+                    href={props.data.audio}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <span className="material-symbols-outlined">volume_up</span>
                 </a>
             </button>
-            <h4 className="ms-1">{props.data.text}</h4>
+            <h4>{props.data.text}</h4>
         </div>
     );
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Synonyms(props) {
     if (props.synonyms) {
         return (
-            <div className="Synonyms">
+            <div className="Synonyms d-flex justify-content-center">
                 {props.synonyms.map(function (synonym, index) {
-                    return <p className="mb-0"key={index}>{synonym}</p>;
+                    return <p className="mb-0 ps-2"key={index}>{synonym}</p>;
                 })}
             </div>
         );
