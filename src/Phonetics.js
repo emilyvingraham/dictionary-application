1aaf59010310f546b704447da3c181a37a995261
Emily Ingraham <emilyvingraham@gmail.com>
2024-03-06 14:35:23 -0700

    Added listen component and bootstrap styling to Phonetics component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Phonetics(props) {
     return (
-        <div className="Phonetics">
-            <h4>{props.data.text}</h4>
+        <div className="Phonetics d-flex justify-content-center my-2">
+            <button className="px-2 me-1 rounded-pill">▶️
+                <a className="fs-6 text-decoration-none" href={props.data.audio} target="_blank" rel="noreferrer">
+                    Listen
+                </a>
+            </button>
+            <h4 className="ms-1">{props.data.text}</h4>
         </div>
-    )
+    );
 }
